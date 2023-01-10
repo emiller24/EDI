2bf42e2e7800d25883ba0f7bf0e68745eae466db
emiller24 <ellaanise@gmail.com>
2023-01-10 13:22:56 -0500

    update

--- CreativeCoding/Variables_gif/sketch.js
+++ CreativeCoding/Variables_gif/sketch.js
@@ -1,21 +1,22 @@
+let gif;
+function preload () {
+  gif = loadImage ('assets/MillerBackground.gif');
+}
 function setup() { //runs one time
   createCanvas (windowWidth,windowWidth);//canvas is 720 pixels x 720 pixels
   background ('lavender'); // background is rbg pink
 }
 
 function draw() {//runs in a loop
-fill ('lavender');
-strokeWeight (1);
+//strokeWeight (1);
 var num = 4 ;
 var sidelen = windowWidth/num ;
 
 
 for (var y = 0; y < windowWidth; y = y + sidelen){ // loop creates a row in the y direction
 for (var x = 0; x < windowWidth; x = x + sidelen){ // loop creates a row in the x direction
-  quad (x,y,
-      x+sidelen,y,
-      x+sidelen,y+sidelen,
-      x, y +sidelen);
+
+  image (gif,x,y,windowWidth/num,windowWidth/num,);
 }
 }
 }
